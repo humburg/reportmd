d3e724afe8b8665663c21a6601f2b0d8dc3133ee
Peter Humburg <p.humburg@garvan.org.au>
2016-03-15 20:51:45 +1100

    Add documentation.

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,23 @@
+#' Replace null values with default
+#'
+#' @param x Variable to be tested.
+#' @param y Default to use if \code{x} is \code{NULL}.
+#' @return If \code{x} is \code{NULL}, \code{y} is returned, otherwise \code{x}.
+#' @name null-or
+#' @author Peter Humburg
 #' @export
 `%||%` <- function(x, y){
   if(is.null(x)) y else x
 }
 
+#' Merge two named lists
+#'
+#' @param x Base list
+#' @param y Additional entries to be merged into \code{x}
+#'
+#' @note Entries in \code{x} that are also present in \code{y}
+#' will use the values from \code{y}.
+#' @return A list with entries from \code{x} and \code{y}.
 #' @export
 merge_list <- function(x,y){
   if(!is.null(y) && length(y)){

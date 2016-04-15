7d4265f5ed9c9b6e92687427d27eab9ae559779a
Peter Humburg <peter.humburg@gmail.com>
2016-04-15 10:01:19 +1000

    Store information about cache location.

--- R/classes.R
+++ R/classes.R
@@ -8,6 +8,8 @@
 #' relative to the location of the main document.
 #' @param source Path to source (i.e. RMarkdown) file used to generate \code{document}.
 #' @param title The title of the document.
+#' @param cache Path to cache directory. If this is missing an attempt will be made to guess
+#' the location if \code{source} is present.
 #' @param ... Additional arguments are ignored.
 #'
 #' @details The \code{source} and \code{title} arguments may be omitted. If
@@ -19,9 +21,10 @@
 #'  \item{document}{The path to the output document.}
 #'  \item{source}{The source document used to generate the output (may be \code{NULL}).}
 #'  \item{title}{The document title. If no title is available this will be 'Untitled'}
+#'  \item{cache}{Location of the cache directory.}
 #' @export
 #' @importFrom stringr str_extract
-Dependency <- function(label, document, source, title, ...){
+Dependency <- function(label, document, source, title, cache, ...){
   dep <- list(label=label, document=document)
   doc_format <- tolower(stringr::str_extract(document, '[^.]+$'))
   if(!missing(source)){
@@ -42,6 +45,16 @@ Dependency <- function(label, document, source, title, ...){
     }
     dep$title <- extracted_title
   }
+
+  if(missing(cache)){
+    if(!is.null(dep$source)){
+      prefix <- sub("\\.[^.]+$", "", dep$source)
+      cache <- file.path(paste(prefix, 'cache', sep="_"), opts_knit$get("rmarkdown.pandoc.to"))
+    } else{
+      cache <- NULL
+    }
+  }
+  dep$cache <- cache
 
   class(dep) <- 'Dependency'
   dep
